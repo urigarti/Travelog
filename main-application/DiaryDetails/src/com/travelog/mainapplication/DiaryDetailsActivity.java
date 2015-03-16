03c390f3ccd6eb0022d9725fdb4eea5326c24bea
urigarti <urigarti@gmail.com>
2015-03-17 01:08:54 +0200

    Initial Commit

---
+++ main-application/DiaryDetails/src/com/travelog/mainapplication/DiaryDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.travelog.mainapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiaryDetailsActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
