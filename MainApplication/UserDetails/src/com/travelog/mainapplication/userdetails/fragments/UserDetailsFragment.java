bfb4c1c9aef314f288b575ac5a413149f0542fef
urigarti <urigarti@gmail.com>
2015-03-21 19:56:42 +0200

    Rename package - type

--- MainApplication/UserDetails/src/com/travelog/mainapplication/userdetails/fragments/UserDetailsFragment.java
+++ MainApplication/UserDetails/src/com/travelog/mainapplication/userdetails/fragments/UserDetailsFragment.java
@@ -1,8 +1,7 @@
-package com.travelog.mainapplication.userdetails.fragements;
+package com.travelog.mainapplication.userdetails.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
