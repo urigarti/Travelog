4c2e96399b67e926787bc9445d0b304a768927fe
urigarti <urigarti@gmail.com>
2015-03-28 09:19:58 +0300

    Resource renaming

--- MainApplication/UserDetails/src/utils/enums/buttons/MainMenuButtons.java
+++ MainApplication/UserDetails/src/utils/enums/buttons/MainMenuButtons.java
@@ -7,15 +7,15 @@ import com.travelog.mainapplication.userdetails.R;
  */
 public enum MainMenuButtons {
 
-    mainPanelHome(R.id.mainPanelHome),
-    mainPanelMap(R.id.mainPanelMap),
-    mainPanelUserDetails(R.id.mainPanelUserDetails),
-    mainPanelEditDiary(R.id.mainPanelEditDiary),
-    mainPanelSorting(R.id.mainPanelSorting),
-    mainPanelAddToDiary(R.id.mainPanelAddToDiary),
-    mainPanelSelections(R.id.mainPanelSelections),
-    mainPanelSharing(R.id.mainPanelSharing),
-    mainPanelDeletions(R.id.mainPanelDeletions);
+    mainPanelHome(R.id.main_menu_home),
+    mainPanelMap(R.id.main_menu_map),
+    mainPanelUserDetails(R.id.main_menu_user_profile),
+    mainPanelEditDiary(R.id.main_menu_edit_diary),
+    mainPanelSorting(R.id.main_menu_sort),
+    mainPanelAddToDiary(R.id.main_menu_add_to_diary),
+    mainPanelSelections(R.id.main_menu_selection),
+    mainPanelSharing(R.id.main_menu_share),
+    mainPanelDeletions(R.id.main_menu_delete);
 
     int buttonId;
 
